003819b4f6020e8c996a01702c4b99f13c382cb4
Lilly Rizvi <rizvi.mu@northeastern.edu>
2024-01-30 15:59:18 -0500

    synth folder

--- build.py
+++ build.py
@@ -1,11 +1,23 @@
+import setuptools, os
+from amaranth_boards.ice40_up5k_b_evn import ICE40UP5KBEVNPlatform
 from rtl.Blinky import *
 
-from amaranth_boards.ice40_up5k_b_evn import ICE40UP5KBEVNPlatform
+def build(setup_kwargs) -> int:
+    # https://github.com/python-poetry/poetry/issues/2051#issuecomment-942049444
+    setuptools.setup(
+        **setup_kwargs, 
+        script_args = ['bdist_wheel'],
+        options = { 
+            'bdist_wheel': {
+                'plat_name': os.getenv('PP_PYTHON_TARGET', 'any')
+            }
+        }
+    )
 
-def build(setup_kwargs):
     ICE40UP5KBEVNPlatform().build(
         Blinky(),
-        synth_opts="-abc9 -dff -spram -dsp",
+        synth_opts='-abc9 -dff -spram -dsp',
+        build_dir='synth',
         do_program=False,
         verbose=True,
     )
